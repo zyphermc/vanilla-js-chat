ed3a24cd5659d52ff4cafa4b442ccfac862e75d4
Richard <hiimrichardd@gmail.com>
2023-03-02 13:27:09 +0800

    Front-end complete
    - Next:
      - Database connection
      - Abstraction
      - Functionality

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnyu3-wtyuNAFTBkG_hR4T2FSYK30Qz08",
+  authDomain: "vanilla-js-chat-998b6.firebaseapp.com",
+  projectId: "vanilla-js-chat-998b6",
+  storageBucket: "vanilla-js-chat-998b6.appspot.com",
+  messagingSenderId: "607570434344",
+  appId: "1:607570434344:web:1bb9d0fb9d97efc3f6181d",
+  measurementId: "G-0PDSY7TEPR",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
